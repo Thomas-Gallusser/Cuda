3aad8e05b9fbea7e8c9c6b39385bb46b42c91a08
Thomas Gallusser <thomas.gallusser@gmail.com>
2019-09-08 02:38:32 +0200

    Suppression des images de la navigation des blogs

--- blog.js
+++ blog.js
@@ -1,48 +1,40 @@
 function selectPortfiolio(select) {
+	document.getElementById("btnAll").style.backgroundColor = "black";
+	document.getElementById("btnWeb").style.backgroundColor = "black";
+	document.getElementById("btnApps").style.backgroundColor = "black";
+	document.getElementById("btnIcons").style.backgroundColor = "black";
+	
+	document.getElementById("web").style.display = "inline-block";
+	document.getElementById("app").style.display = "inline-block";
+	document.getElementById("playersui").style.display = "inline-block";
+	document.getElementById("icons").style.display = "inline-block";
+	
 	switch (select) {
 		case 0:
-			document.getElementById("btnAll").style.backgroundImage = "url('images/select.png')";
-			document.getElementById("btnWeb").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnApps").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnIcons").style.backgroundImage = "url('images/unselect.png')";
-      		document.getElementById("web").style.display = "inline-block";
-      		document.getElementById("app").style.display = "inline-block";
-      		document.getElementById("playersui").style.display = "inline-block";
-      		document.getElementById("icons").style.display = "inline-block";
+      		document.getElementById("btnAll").style.backgroundColor = "white";
 			break;
+			
 		case 1:
-			document.getElementById("btnAll").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnWeb").style.backgroundImage = "url('images/select.png')";
-			document.getElementById("btnApps").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnIcons").style.backgroundImage = "url('images/unselect.png')";
+			document.getElementById("btnWeb").style.backgroundColor = "white";
       
-      		document.getElementById("web").style.display = "inline-block";
-      		document.getElementById("app").style.display = "none";
-      		document.getElementById("playersui").style.display = "inline-block";
-      		document.getElementById("icons").style.display = "none";
+			document.getElementById("app").style.display = "none";
+			document.getElementById("icons").style.display = "none";
 			break;
+			
 		case 2:
-			document.getElementById("btnAll").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnWeb").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnApps").style.backgroundImage = "url('images/select.png')";
-			document.getElementById("btnIcons").style.backgroundImage = "url('images/unselect.png')";
+			document.getElementById("btnApps").style.backgroundColor = "white";
       
-      		document.getElementById("web").style.display = "none";
-      		document.getElementById("app").style.display = "inline-block";
-      		document.getElementById("playersui").style.display = "none";
-      		document.getElementById("icons").style.display = "none";
-      		
+			document.getElementById("web").style.display = "none";
+			document.getElementById("playersui").style.display = "none";
+			document.getElementById("icons").style.display = "none";
 			break;
+			
 		case 3:
-			document.getElementById("btnAll").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnWeb").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnApps").style.backgroundImage = "url('images/unselect.png')";
-			document.getElementById("btnIcons").style.backgroundImage = "url('images/select.png')";
-      
-      		document.getElementById("web").style.display = "none";
-      		document.getElementById("app").style.display = "none";
-      		document.getElementById("playersui").style.display = "none";
-      		document.getElementById("icons").style.display = "inline-block";  		
+			document.getElementById("btnIcons").style.backgroundColor = "white";
+      	
+			document.getElementById("web").style.display = "none";
+			document.getElementById("app").style.display = "none";
+			document.getElementById("playersui").style.display = "none";		
 			break;
 	}
 }
